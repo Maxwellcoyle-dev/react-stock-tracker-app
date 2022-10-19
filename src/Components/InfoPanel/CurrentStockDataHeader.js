1603fc991faebe54feeb4d57a6a3bb01c2a25d5e
Maxwellcoyle-dev <maxwellc.henderson@gmail.com>
2022-10-19 09:25:47 -0400

    fixed favorotes list to not allow duplicates

--- src/Components/InfoPanel/CurrentStockDataHeader.js
+++ src/Components/InfoPanel/CurrentStockDataHeader.js
@@ -4,24 +4,30 @@ import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 import styles from "./PanelStyles.module.css";
 
 export const CurrentStockDataHeader = (props) => {
-  const { searchParam, summaryData, searchParamLogo } =
-    useContext(searchContext);
+  const { summaryData, searchParamLogo } = useContext(searchContext);
 
+  // Add a new favorite to the favorite list
   const addFav = (event) => {
     event.preventDefault();
-
-    props.setFavorites([
-      ...props.favorites,
-      {
-        symbol: summaryData?.symbol,
-        price: summaryData?.price.regularMarketPrice.fmt,
-        color: summaryData?.price.regularMarketChange.raw > 0 ? "green" : "red",
-        regMarketChange: summaryData?.price.regularMarketChange.fmt,
-        regMarketChangePercent:
-          summaryData?.price.regularMarketChangePercent.fmt,
-        id: Math.random() * 1000,
-      },
-    ]);
+    // New Favorite item to be added
+    const newFav = {
+      symbol: summaryData?.symbol,
+      price: summaryData?.price.regularMarketPrice.fmt,
+      color: summaryData?.price.regularMarketChange.raw > 0 ? "green" : "red",
+      regMarketChange: summaryData?.price.regularMarketChange.fmt,
+      regMarketChangePercent: summaryData?.price.regularMarketChangePercent.fmt,
+      id: Math.random() * 1000,
+    };
+    // Check if the stock already exists in the favorites list
+    const checkDuplicate = props.favorites.some(
+      (fav) => fav.symbol === summaryData?.symbol
+    );
+    // If the stock does not already exist in the favorites list or if the favorites list is empty, then add the stock to the favorites list
+    if (!checkDuplicate) {
+      props.setFavorites([...props.favorites, newFav]);
+    } else if (props.favorites.length < 1) {
+      props.setFavorites([...props.favorites, newFav]);
+    }
   };
 
   return (
